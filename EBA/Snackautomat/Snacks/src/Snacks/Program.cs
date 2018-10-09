6a00b4a34469d28696e101a63ae8c3638627bffa
Zeroks77 <40673554+Zeroks77@users.noreply.github.com>
2018-10-09 09:32:56 +0200

    Update Program.cs

--- EBA/Snackautomat/Snacks/src/Snacks/Program.cs
+++ EBA/Snackautomat/Snacks/src/Snacks/Program.cs
@@ -19,38 +19,6 @@ namespace Snacks
             Application.Run(new Form1());
         }
     }
-       static class Hunger
-    {
-        public static int hungry(int hungervalue)
-        {
-            switch (hungervalue) //je hungriger desto mehr essen
-            {
-                case 0:
-             return 2;
-                case 1:
-             return 1;
-                case 2:
-             return 0;
-                default:
-             return -1;
-            }
-            
-        }
-        public static int weight(int weightvalue)
-        {
-            switch (weightvalue) //um so schwerer desto weniger || Index ist falsch rum .... 
-            {
-                case 0:
-                    return 0;
-                case 1:
-                    return 1;
-                case 2:
-                    return 2;
-                default:
-                    return -1;
-            }
-           
-        }
-        
-    }
+     
+    
 }
