3ead1fb5df78d78f5fa1268f157de6658baa21bc
Zeroks77 <dominik@mangatter.de>
2019-03-28 11:00:42 +0100

    Zahlenraten Version 1

---
+++ Start2Code/Schleifen&verzweigungen/Schleifen und Verzweigungen/Schleifen und Verzweigungen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Schleifen_und_Verzweigungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, hier möchte ich zeigen, wie man mit Schleifen und Verzweigungen umgehen kann");
+            Console.WriteLine("Dafür lass uns doch ein Spielen. Wie wäre es mit Zahlenraten, ich denke mir eine Zahl von 1 bis 100 aus und du musst sie erraten :)");
+            Console.Write("Ok dann dein erster versuch:");
+            Zahlenraten();
+
+
+        }
+
+        public static void Zahlenraten()
+        {
+            int guesses = 0;
+            int guess = 0;
+            int mynumber = new Random().Next(1,101);
+            bool checkinput = false;
+
+            bool right = false;
+            while (!right)
+            {
+                checkinput = int.TryParse(Console.ReadLine(), out guess);
+                if (!checkinput)
+                {
+                    Console.WriteLine("Gib doch bitte eine Zahl ein :)");
+                    continue;
+                }
+                guesses++;
+                if (guess > mynumber)
+                {
+                    Console.WriteLine("Deine Zahl ist zu groß");
+                    continue;
+                }
+                if (guess < mynumber)
+                {
+                    Console.WriteLine("Deine Zahl ist zu klein");
+                    continue;
+                }
+                if (guess == mynumber)
+                {
+                    Console.WriteLine($"Coole Sache, du hast es geschafft meine Zahl {mynumber} in nur {guesses} Versuchen zu erraten");
+                    right = true;
+                }
+            }
+         }
+    }
+}
